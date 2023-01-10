c71259399ade47c00747be87b322e92a2e995b8a
Higo Souza <higoedu@gmail.com>
2023-01-10 19:47:06 -0300

    comentário vetor 2
    comentário vetor 2

--- src/estruturadadosvetor/Vetor.java
+++ src/estruturadadosvetor/Vetor.java
@@ -2,11 +2,15 @@ package estruturadadosvetor;
 /*
 um vetor (ou array) é a estrutura de dados mais simples que existe
 um vetor armazena uma sequência de valores onde todos são do mesmo tipo
+
+- criou a classe vetor
+- criou um construtor, para poder inicializar o vetor
 */
 public class Vetor {
     private String[] elementos;
     //construtor
     public Vetor(int capacidade){
+        //atributo elemento foi instanciado e teve 5 posições
         this.elementos = new String[capacidade];
     }
 }
