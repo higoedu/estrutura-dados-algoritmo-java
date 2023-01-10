4938e39e3ed34aa4b0b9688f9398e94669d9e72a
Higo Souza <higoedu@gmail.com>
2023-01-10 20:16:15 -0300

    criou método adiciona elemento
    criou método adiciona elemento

--- src/estruturadadosvetor/Vetor.java
+++ src/estruturadadosvetor/Vetor.java
@@ -5,6 +5,8 @@ um vetor armazena uma sequência de valores onde todos são do mesmo tipo
 
 - criou a classe vetor
 - criou um construtor, para poder inicializar o vetor
+
+- criou método para adicionar um elemento
 */
 public class Vetor {
     private String[] elementos;
@@ -12,5 +14,15 @@ public class Vetor {
     public Vetor(int capacidade){
         //atributo elemento foi instanciado e teve 5 posições
         this.elementos = new String[capacidade];
+    }
+
+    //adicionar um elemento no final do vetor
+    public void adiciona(String elemento){
+        for(int i = 0; i < this.elementos.length; i++){
+            if(this.elementos[i] == null){
+                this.elementos[i] = elemento;
+                break;
+            }
+        }
     }
 }
---
+++ src/estruturadadosvetorteste/Aula3.java
@@ -0,0 +1,12 @@
+package estruturadadosvetorteste;
+
+import estruturadadosvetor.Vetor;
+
+public class Aula3 {
+    public static void main(String[] args) {
+        Vetor vetor = new Vetor(5);
+
+        vetor.adiciona("elemento 1");
+        vetor.adiciona("elemento 2");
+    }
+}
