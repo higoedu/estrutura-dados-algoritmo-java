f1681dde115dfd1e863e3ab9a70b9d9accd75bdc
Higo Souza <higoedu@gmail.com>
2023-01-10 19:41:31 -0300

    comentário vetor
    comentário vetor

--- src/estruturadadosvetor/Vetor.java
+++ src/estruturadadosvetor/Vetor.java
@@ -1,5 +1,8 @@
 package estruturadadosvetor;
-
+/*
+um vetor (ou array) é a estrutura de dados mais simples que existe
+um vetor armazena uma sequência de valores onde todos são do mesmo tipo
+*/
 public class Vetor {
     private String[] elementos;
     //construtor
