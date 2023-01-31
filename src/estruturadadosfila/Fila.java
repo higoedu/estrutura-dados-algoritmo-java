f8e98b1d4abcec9de55720a1ac2da6e83bc7219c
higo <higoedu@gmail.com>
2023-01-30 21:56:15 -0300

    criou classe fila
    criou classe fila

---
+++ src/estruturadadosfila/Fila.java
@@ -0,0 +1,13 @@
+package estruturadadosfila;
+
+import estruturadadosvetor.EstruturaEstatica;
+
+public class Fila<T> extends EstruturaEstatica<T> {
+    public Fila(){
+        super();
+    }
+
+    public Fila(int capacidade){
+        super(capacidade);
+    }
+}
