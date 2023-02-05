3ac43d91c798272a75f9e075d0ec678c712b0f9b
higo <higoedu@gmail.com>
2023-02-04 22:35:31 -0300

    mostrando em forma de array
    mostrando em forma de array

--- src/estruturadadoslista/ListaEncadeada.java
+++ src/estruturadadoslista/ListaEncadeada.java
@@ -25,9 +25,34 @@ public class ListaEncadeada<T> {
 
     @Override
     public String toString() {
-        StringBuilder stringBuilder = new StringBuilder();
+        StringBuilder stringBuilder = new StringBuilder("[");
 
-        stringBuilder.append("ListaEncadeada [inicio=").append(inicio).append("]");
+        //[1, 2, 3, 4,]
+        if(this.tamanho == 0){
+            return "[]";
+        }
+
+        //stringBuilder.append("ListaEncadeada [inicio=").append(inicio).append("]");
+
+        No<T> atual = this.inicio;
+
+        for(int i = 0; i < this.tamanho - 1; i++){
+            stringBuilder.append(atual.getElemento()).append(", ");
+
+            atual = atual.getProximo();
+        }
+
+        stringBuilder.append(atual.getElemento()).append("]");
+
+        /*
+        stringBuilder.append(atual.getElemento()).append(", ");
+
+        while (atual.getProximo() != null){
+            atual = atual.getProximo();
+
+            stringBuilder.append(atual.getElemento()).append(", ");
+        }
+        */
 
         return stringBuilder.toString();
     }
